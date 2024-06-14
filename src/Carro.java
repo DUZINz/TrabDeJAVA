afe6a982972905d386e4b6e07c13abedcafcbe74
DUZINz <eduardogrunitzky@gmail.com>
2024-06-14 11:34:30 -0300

    classe do carro

--- src/Carro.java
+++ src/Carro.java
@@ -1,2 +1,41 @@
 public class Carro {
+    private String nome;
+    private String modelo;
+    private int ano;
+
+    public Carro (String nome, String modelo, int ano) {
+        this.nome = nome;
+        this.modelo = modelo;
+        this.ano = ano;
+        }
+
+        public String getNome(){
+            return nome;
+        }
+        public void setNome(String nome){
+            this.nome = nome;
+        }
+
+        public String getModelo(){
+            return modelo;
+        }
+        public void setModelo(String modelo){
+            this.modelo = modelo;
+        }
+
+        public int getAno(){
+            return ano;
+        }
+        public void setAno(int ano){
+            this.ano = ano;
+        }
+
+        @Override
+        public String toString(){
+            return "Carro{" +
+            "nome='" + nome + '\'' +
+            "modelo='" + modelo + '\'' +
+            "ano='" + ano + '\'' +
+            '}'; 
+        }
 }
