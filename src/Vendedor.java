63edb72ad5445a5f0d370e7577e7b2500902f71e
Matheus Henrique Rodrigues Gadonski <142113295+Gadonski05@users.noreply.github.com>
2024-06-18 22:42:15 -0300

    Vendedor

--- src/Vendedor.java
+++ src/Vendedor.java
@@ -1,2 +1,7 @@
-public class Vendedor {
+public class Vendedor extends Pessoa{
+
+    public Vendedor(String nome, String cpf, String endereco, String email, String telefone_Celular) {
+        super(nome, cpf, endereco, email, telefone_Celular);
+        
+    }
 }
