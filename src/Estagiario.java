4987efa7a44423a4aff1ae1987a2e1748ea89c83
Matheus Henrique Rodrigues Gadonski <142113295+Gadonski05@users.noreply.github.com>
2024-06-18 22:40:32 -0300

    Início
    Iniciando á classe dos estagiários.

--- src/Estagiario.java
+++ src/Estagiario.java
@@ -1,2 +1,7 @@
-public class Estagiario {
+public class Estagiario extends Pessoa{
+
+    public Estagiario(String nome, String cpf, String endereco, String email, String telefone_Celular) {
+        super(nome, cpf, endereco, email, telefone_Celular);
+        
+    }
 }
