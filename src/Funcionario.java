86fb204cddb11d3a6f9ec1ac3f5f65aa9434ac9d
Matheus Henrique Rodrigues Gadonski <142113295+Gadonski05@users.noreply.github.com>
2024-06-18 22:41:24 -0300

    Funcionários
    Iniciando á classe funcionários.

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -1,2 +1,7 @@
-public class Funcionario {
+public class Funcionario extends Pessoa{
+
+    public Funcionario(String nome, String cpf, String endereco, String email, String telefone_Celular) {
+        super(nome, cpf, endereco, email, telefone_Celular);
+        
+    }
 }
