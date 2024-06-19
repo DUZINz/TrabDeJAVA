3340435a976ba28336ba3fc6f67d9830cb0cbee6
DUZINz <eduardogrunitzky@gmail.com>
2024-06-19 17:01:18 -0300

    att pecas

--- src/Pecas.java
+++ src/Pecas.java
@@ -1,7 +1,45 @@
 public class Pecas extends Estoque{
-
-    public Pecas(String id, String nome, String descrição, Float valor_Custo) {
-        super(id, nome, descrição, valor_Custo);
-        
+    private String id;
+    private String nome;
+    private String description;
+    private Float valor_custo;
+        public Pecas(String id, String nome, String descrição, Float valor_Custo) {
+            super(id, nome, descrição, valor_Custo);
+            id = id;
+            nome = nome;
+            this.description = description;
+            valor_custo = valor_custo;
+        }
+        public String getId() {
+            return id;
+        }
+        public void setId(String id) {
+            this.id = id;
+        }
+        public String getNome() {
+            return nome;
+        }
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+        public String getDescription() {
+            return description;
+        }
+        public void setDescription(String description) {
+            this.description = description;
+        }
+        public Float getValor_custo() {
+            return valor_custo;
+        }
+        public void setValor_custo(Float valor_custo) {
+            this.valor_custo = valor_custo;
+        }
+        @Override
+        public String toString() {
+            return "Pecas{" +
+                    "ID='" + id + '\'' +
+                    ", Nome ='" + nome + '\'' +
+                    ", Decription='" + description + '\'' +
+                    ", Valor_Custo='" + valor_custo + '\''+'}';
     }
-}
+    }
