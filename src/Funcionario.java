345f87939ef91632047917f794bb98eb538c9ff6
Matheus Henrique Rodrigues Gadonski <142113295+Gadonski05@users.noreply.github.com>
2024-06-20 17:07:32 -0300

    Update Funcionario.java

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -1,7 +1,7 @@
 public class Funcionario extends Pessoa{
 
     public Funcionario(String nome, String cpf, String endereco, String email, String telefone_Celular) {
-        super(nome, cpf, endereco, email, telefone_Celular);
+        super(nome, cpf, idade, endereco, email, telefone_Celular);
         
     }
 }
