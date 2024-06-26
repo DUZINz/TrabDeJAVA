3d9ce2a3ee683aa34a04ecb94066e0ab4ceb6294
Matheus Henrique Rodrigues Gadonski <142113295+Gadonski05@users.noreply.github.com>
2024-06-26 17:03:22 -0300

    New class
    Criação da classe caminhonete.

---
+++ src/Caminhonete.java
@@ -0,0 +1,60 @@
+public class Caminhonete extends Veiculo{
+
+    private int numchassi;
+    private String nome;
+    private String modelo;
+    private int ano;
+
+    public Caminhonete(String nome, String modelo, int ano, int numchassi){
+       super(nome, modelo, ano, numchassi, modelo);
+
+       this.nome = nome;
+       this.modelo = modelo;
+       this.ano = ano;
+       this.numchassi = numchassi;
+
+    }
+
+    public int getNumchassi() {
+        return numchassi;
+    }
+
+    public void setNumchassi(int numchassi) {
+        this.numchassi = numchassi;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    @Override
+    public String toString() {
+        return "Carro{" +
+                ", nome='" + nome + '\'' +
+                ", modelo='" + modelo + '\'' +
+                ", ano='" + ano + '\'' +
+                ", Número do chassi='" + numchassi + '\'' +
+                '}';
+    
+    }
+}
